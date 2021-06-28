2a5d37b91670b452593c768d447c150262fa8729
Woipot <frostik0409@gmail.com>
2021-06-28 17:38:09 +0300

    worked version

--- discord_telegram_site_check_bot/DbManager.py
+++ discord_telegram_site_check_bot/DbManager.py
@@ -5,7 +5,7 @@ class DbConnectionManager():
 
     def __init__(self) -> None:
         super().__init__()
-        self.db = sqlite3.connect('db/url.db', check_same_thread=False)
+        self.db = sqlite3.connect('url.db', check_same_thread=False)
         self.sql = self.db.cursor()
         self.url_repo = UrlsBdRepository(self.db)
         self.sql.execute("""CREATE TABLE IF NOT EXISTS users (
